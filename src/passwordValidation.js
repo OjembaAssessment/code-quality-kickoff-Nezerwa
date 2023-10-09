12818a47e0a35365d026ccc02ba77cc4284760f1
Nezerwa <eligrand2000@gmail.com>
2023-10-09 18:30:29 +0200

    Submit the password validator exercise

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -9,18 +9,11 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
 export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
-
-  // * * * YOUR CODE GOES IN HERE ... * * *
-  /*
-   * if (password is not exactly 10 digits or characters...) {
-   *   return ...;
-   * }
-   *
-   * if (is not composed by digits and numbers) {
-   *   return ...;
-   * }
-   */
+  if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-z-A-Z-0-9]{10}$/g.test(password))
+    return false;
+  if (/01|12|23|34|45|56|67|78|89|98|87|76|65|54|43|32|21|10/g.test(password)) return false;
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
+  if(forbiddenPasswords.includes(password)) return false
   return true;
 }
