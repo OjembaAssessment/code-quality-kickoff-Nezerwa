f17595a5264161ca63aebb14624f3406ade4f871
Nezerwa <eligrand2000@gmail.com>
2023-10-09 19:18:35 +0200

    Change regular function to arrow function

--- src/improvement-1.js
+++ src/improvement-1.js
@@ -3,10 +3,9 @@
  *  YOU CAN REPLACE THE CODE INSIDE THE FUNCTIONS, THE FUNCTION NAME, ETC.
  *  PLEASE DO NOT REMOVE `export default function`
  */
-export default function countLetterR(string) {
+export default countLetterR = (string) => {
   const newArray = string.match(/R/g);
-  return newArray.length
-  
-}
+  return newArray.length;
+};
 
 console.log(countLetterR("abcdefRRR1234"));
