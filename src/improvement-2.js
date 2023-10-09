c410dd642229cdd9216b55b26f7bb010154ba885
Nezerwa <eligrand2000@gmail.com>
2023-10-09 20:58:00 +0200

    Use replace loop with fiter method to improve readability

--- src/improvement-2.js
+++ src/improvement-2.js
@@ -5,13 +5,7 @@
  */
 
 // FUNCTION 2
-export default function processItems(items) {
-  const processedItems = [];
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    if (item.type === "Food") {
-      processedItems.push(item);
-    }
-  }
+export default processItems = (items) => {
+  const processedItems = items.filter((item) => item.type === "Food");
   return processedItems;
-}
+};
