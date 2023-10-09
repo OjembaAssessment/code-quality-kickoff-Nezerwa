a54ad6d3690b7817def6c85432d73eab61c35923
Nezerwa <eligrand2000@gmail.com>
2023-10-09 21:25:23 +0200

    Use sort and get the first one in array to get the largest

--- src/improvement-3.js
+++ src/improvement-3.js
@@ -5,12 +5,5 @@
  */
 
 // FUNCTION 3
-export default function findLargestNumber(numbers) {
-  let largest = numbers[0];
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > largest) {
-      largest = numbers[i];
-    }
-  }
-  return largest;
-}
+export default findLargestNumber = (numbers) => numbers.sort((a, b) => b - a)[0];
+
